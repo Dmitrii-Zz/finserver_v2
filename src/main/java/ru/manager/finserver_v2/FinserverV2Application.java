8a7750a3931193e2d3bd91fec03106e338643a7c
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-08-21 22:39:28 +0700

    Вторая версия финсервера

---
+++ src/main/java/ru/manager/finserver_v2/FinserverV2Application.java
@@ -0,0 +1,13 @@
+package ru.manager.finserver_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinserverV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinserverV2Application.class, args);
+	}
+
+}
