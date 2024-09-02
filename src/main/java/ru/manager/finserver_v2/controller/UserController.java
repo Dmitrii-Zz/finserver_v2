c281b2fbc073099fc1508676516e8e168ff3a4c0
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-02 22:32:27 +0700

    Добавление пользователя

--- src/main/java/ru/manager/finserver_v2/controller/UserController.java
+++ src/main/java/ru/manager/finserver_v2/controller/UserController.java
@@ -1,8 +1,16 @@
 package ru.manager.finserver_v2.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/finserver/users")
 public class UserController {
+
+    @PostMapping
+    public String createUser() {
+        return "Создаём пользователя";
+    }
 
 }
---
+++ src/main/java/ru/manager/finserver_v2/model/User.java
@@ -0,0 +1,6 @@
+package ru.manager.finserver_v2.model;
+
+public class User {
+    private Long userId;
+    private String name;
+}
