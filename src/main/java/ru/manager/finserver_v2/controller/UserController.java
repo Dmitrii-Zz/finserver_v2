283be39ad05c648958717108fc758561491ce68e
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-03 22:49:48 +0700

    Операции Post Delete Pacth

--- src/main/java/ru/manager/finserver_v2/controller/UserController.java
+++ src/main/java/ru/manager/finserver_v2/controller/UserController.java
@@ -1,16 +1,33 @@
 package ru.manager.finserver_v2.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.constraints.Positive;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.manager.finserver_v2.dto.UserDto;
 
+@Slf4j
+@Validated
 @RestController
 @RequestMapping("/finserver/users")
 public class UserController {
 
     @PostMapping
-    public String createUser() {
-        return "Создаём пользователя";
+    public UserDto createUser(@RequestBody @Validated UserDto userDto) {
+        log.info("Запрос на создание пользователя с именем: '{}'", userDto.getName());
+        return userDto;
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable @Positive long userId) {
+        log.info("Запрос на удаление пользователя id = '{}'", userId);
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto updateUser(@PathVariable @Positive long userId,
+                              @RequestBody @Validated UserDto userDto) {
+        log.info("Запрос на обновление пользователя id = '{}'", userId);
+        return userDto;
     }
 
 }
