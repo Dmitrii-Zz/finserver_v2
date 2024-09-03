1e317ac01143d351c4015234bd661bb23a79c97b
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-03 21:59:00 +0700

    добавить БД пользователя

--- src/main/java/ru/manager/finserver_v2/model/User.java
+++ src/main/java/ru/manager/finserver_v2/model/User.java
@@ -1,6 +1,15 @@
 package ru.manager.finserver_v2.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    @Column(nullable = false, length = 50)
     private String name;
 }
