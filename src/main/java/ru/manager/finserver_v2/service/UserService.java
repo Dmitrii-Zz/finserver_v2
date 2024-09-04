895b6eb1859fa499c4a6045f5e150ea439ec1fca
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-04 22:17:07 +0700

    создать интерфейс user service

---
+++ src/main/java/ru/manager/finserver_v2/service/UserService.java
@@ -0,0 +1,10 @@
+package ru.manager.finserver_v2.service;
+
+import ru.manager.finserver_v2.dto.UserDto;
+
+interface UserService {
+    UserDto createUser(UserDto userDto);
+    UserDto updateUser(long userId, UserDto userDto);
+    UserDto getUser(long userId);
+    void deleteUser(long userId);
+}
