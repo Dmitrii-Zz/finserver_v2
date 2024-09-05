1494a5ce512ecbc8b0616d089fe77e88cac6a409
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-05 21:41:55 +0700

    наполнить методы user mapper

--- src/main/java/ru/manager/finserver_v2/mapper/UserMapper.java
+++ src/main/java/ru/manager/finserver_v2/mapper/UserMapper.java
@@ -6,10 +6,15 @@ import ru.manager.finserver_v2.model.User;
 public class UserMapper {
 
     public static User toUser(UserDto userDto) {
-        return null;
+        return User.builder()
+                .name(userDto.getName())
+                .build();
     }
 
     public static UserDto toUserDto(User user) {
-        return null;
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .build();
     }
 }
