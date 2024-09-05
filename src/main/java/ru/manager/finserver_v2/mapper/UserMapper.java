82701bf46b65685f645cc07c9e2ec85af8fa1771
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-05 21:30:32 +0700

    Создать user mapper

---
+++ src/main/java/ru/manager/finserver_v2/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package ru.manager.finserver_v2.mapper;
+
+import ru.manager.finserver_v2.dto.UserDto;
+import ru.manager.finserver_v2.model.User;
+
+public class UserMapper {
+
+    public static User toUser(UserDto userDto) {
+        return null;
+    }
+
+    public static UserDto toUserDto(User user) {
+        return null;
+    }
+}
