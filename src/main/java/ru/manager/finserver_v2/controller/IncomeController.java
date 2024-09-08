a3ab785f1744d29d36a04eb7d8ab20e91fa5f462
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-08 17:31:52 +0700

    Создать скелет контроллера доходов

--- src/main/java/ru/manager/finserver_v2/controller/IncomeController.java
+++ src/main/java/ru/manager/finserver_v2/controller/IncomeController.java
@@ -1,15 +1,35 @@
 package ru.manager.finserver_v2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/finserver/income")
 public class IncomeController {
 
-    @GetMapping
+    @PostMapping
     public String addIncome() {
-        return "Возвращаем что-нибудь";
+        return null;
+    }
+
+    @GetMapping
+    public String getIncome() {
+        return "Возвращаем одну трату";
+    }
+
+    @GetMapping
+    public List<String> getListIncome() {
+        return null;
+    }
+
+    @PatchMapping
+    public String updateIncome() {
+        return null;
+    }
+
+    @DeleteMapping
+    public String deleteIncome() {
+        return null;
     }
 }
