5d91d37f5c7bd63660bfcc06ac6ff452bcb4f9ff
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-08 21:45:26 +0700

    Создать income dto

---
+++ src/main/java/ru/manager/finserver_v2/dto/IncomeDto.java
@@ -0,0 +1,31 @@
+package ru.manager.finserver_v2.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class IncomeDto {
+
+    private Long incomeId;
+
+    @NotNull
+    @Positive
+    private BigDecimal count;
+
+    @NotBlank
+    @Length(max = 100)
+    private String note;
+
+    @NotNull
+    private LocalDate date;
+
+    private UserDto owner;
+}
