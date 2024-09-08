2a01ed1e443b19fe46207e87263a3d3da7f53380
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-08 17:20:47 +0700

    Добавить БД для income

--- src/main/java/ru/manager/finserver_v2/model/Income.java
+++ src/main/java/ru/manager/finserver_v2/model/Income.java
@@ -1,11 +1,32 @@
 package ru.manager.finserver_v2.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Data
+@Builder
+@Table(name = "incomes")
 public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long incomeId;
+
+    @Column(nullable = false)
     private BigDecimal count;
+
+    @Column(nullable = false, length = 100)
     private String note;
+
+    @Column(nullable = false)
     private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User owner;
 }
