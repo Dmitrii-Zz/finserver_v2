7931986a211c6a7bd25db626524803f0f591b3db
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-09 19:05:31 +0700

    Добавить интрефейс income service

---
+++ src/main/java/ru/manager/finserver_v2/service/interfaces/IncomeService.java
@@ -0,0 +1,13 @@
+package ru.manager.finserver_v2.service.interfaces;
+
+import ru.manager.finserver_v2.dto.IncomeDto;
+
+import java.util.List;
+
+public interface IncomeService {
+    IncomeDto addIncome(IncomeDto incomeDto, long userId);
+    IncomeDto getIncome(long incomeId, long  userId);
+    List<IncomeDto> getListIncome(long userId);
+    IncomeDto updateIncome(IncomeDto incomeDto, long incomeId, long userId);
+    void deleteIncome(long incomeId, long userId);
+}
