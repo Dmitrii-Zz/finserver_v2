986b4c77e6370c05e2ae9de892dc90eb8df231b0
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-09 22:45:53 +0700

    Добавить конструкторы в user

--- src/main/java/ru/manager/finserver_v2/model/User.java
+++ src/main/java/ru/manager/finserver_v2/model/User.java
@@ -1,12 +1,16 @@
 package ru.manager.finserver_v2.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
     
