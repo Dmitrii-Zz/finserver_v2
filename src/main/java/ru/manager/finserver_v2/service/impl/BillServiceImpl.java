5ff9dbadc36c99f547e264fa8a5f30018773a7ff
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-11 22:30:25 +0700

    создать скелет bill service

---
+++ src/main/java/ru/manager/finserver_v2/service/impl/BillServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.manager.finserver_v2.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.manager.finserver_v2.dto.BillDto;
+import ru.manager.finserver_v2.repository.BillRepository;
+import ru.manager.finserver_v2.service.interfaces.BillService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BillServiceImpl implements BillService {
+
+    private final BillRepository billStorage;
+    
+    @Override
+    public BillDto createBill(BillDto billDto, long userId) {
+        return null;
+    }
+
+    @Override
+    public BillDto getBillById(long billId, long userId) {
+        return null;
+    }
+
+    @Override
+    public List<BillDto> getAllBill(long userId) {
+        return List.of();
+    }
+
+    @Override
+    public BillDto updateBill(BillDto billDto, long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteBill(long billId, long userId) {
+
+    }
+}
