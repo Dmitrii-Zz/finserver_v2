1db3583eac8e63715fb71cf1a722791784db929d
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-12 22:42:46 +0700

    Наполнить метод get bill by id

--- src/main/java/ru/manager/finserver_v2/service/impl/BillServiceImpl.java
+++ src/main/java/ru/manager/finserver_v2/service/impl/BillServiceImpl.java
@@ -3,25 +3,43 @@ package ru.manager.finserver_v2.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.manager.finserver_v2.dto.BillDto;
+import ru.manager.finserver_v2.dto.UserDto;
+import ru.manager.finserver_v2.mapper.BillMapper;
+import ru.manager.finserver_v2.model.Bill;
 import ru.manager.finserver_v2.repository.BillRepository;
 import ru.manager.finserver_v2.service.interfaces.BillService;
+import ru.manager.finserver_v2.service.interfaces.UserService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BillServiceImpl implements BillService {
 
     private final BillRepository billStorage;
+    private final UserService userServiceImpl;
     
     @Override
     public BillDto createBill(BillDto billDto, long userId) {
-        return null;
+        billDto.setOwner(userServiceImpl.getUser(userId));
+        return BillMapper.toBillDto(billStorage.save(BillMapper.toBill(billDto)));
     }
 
     @Override
     public BillDto getBillById(long billId, long userId) {
-        return null;
+        UserDto userDto = userServiceImpl.getUser(userId);
+        Optional<Bill> billOptional = billStorage.findById(billId);
+
+        if (billOptional.isEmpty()) {
+            throw new RuntimeException("Счёт № '" + billId + "' отсутствует.");
+        }
+
+        if (userDto.getId() != billOptional.get().getOwner().getUserId()) {
+            throw new RuntimeException("Отсутствует доступ к счету № " + billId);
+        }
+
+        return BillMapper.toBillDto(billOptional.get());
     }
 
     @Override
