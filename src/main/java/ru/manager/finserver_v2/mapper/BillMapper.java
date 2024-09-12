c02fad7edd6c824ad6381d409b3278569dcadbc5
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-12 21:52:48 +0700

    создать bill mapper с двумя методами

---
+++ src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
@@ -0,0 +1,25 @@
+package ru.manager.finserver_v2.mapper;
+
+import ru.manager.finserver_v2.dto.BillDto;
+import ru.manager.finserver_v2.model.Bill;
+
+public class BillMapper {
+
+    public static Bill toBill(BillDto billDto) {
+        return Bill.builder()
+                .billId(billDto.getBillId())
+                .count(billDto.getCount())
+                .name(billDto.getName())
+                .owner(UserMapper.toUser(billDto.getOwner()))
+                .build();
+    }
+
+    public static BillDto toBillDto(Bill bill) {
+        return BillDto.builder()
+                .billId(bill.getBillId())
+                .count(bill.getCount())
+                .name(bill.getName())
+                .owner(UserMapper.toUserDto(bill.getOwner()))
+                .build();
+    }
+}
