0e92865622a8b5b5388aff860351a9bfc5a078dc
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-15 22:53:22 +0700

    Revert "Добавить маппер для bill update dto"
    This reverts commit 4e769e290183555f1201346dc6cf20ca80fb421b.

--- src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
+++ src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
@@ -1,7 +1,6 @@
 package ru.manager.finserver_v2.mapper;
 
 import ru.manager.finserver_v2.dto.BillDto;
-import ru.manager.finserver_v2.dto.BillUpdateDto;
 import ru.manager.finserver_v2.model.Bill;
 
 public class BillMapper {
@@ -21,15 +20,6 @@ public class BillMapper {
                 .count(bill.getCount())
                 .name(bill.getName())
                 .owner(UserMapper.toUserDto(bill.getOwner()))
-                .build();
-    }
-
-    public static Bill toBill(BillUpdateDto billUpdateDto) {
-        return Bill.builder()
-                .billId(billUpdateDto.getBillId())
-                .count(billUpdateDto.getCount())
-                .name(billUpdateDto.getName())
-                .owner(UserMapper.toUser(billUpdateDto.getOwner()))
                 .build();
     }
 }
