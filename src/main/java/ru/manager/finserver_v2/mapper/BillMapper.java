4e769e290183555f1201346dc6cf20ca80fb421b
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-15 22:05:27 +0700

    Добавить маппер для bill update dto

--- src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
+++ src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
@@ -1,6 +1,7 @@
 package ru.manager.finserver_v2.mapper;
 
 import ru.manager.finserver_v2.dto.BillDto;
+import ru.manager.finserver_v2.dto.BillUpdateDto;
 import ru.manager.finserver_v2.model.Bill;
 
 public class BillMapper {
@@ -20,6 +21,15 @@ public class BillMapper {
                 .count(bill.getCount())
                 .name(bill.getName())
                 .owner(UserMapper.toUserDto(bill.getOwner()))
+                .build();
+    }
+
+    public static Bill toBill(BillUpdateDto billUpdateDto) {
+        return Bill.builder()
+                .billId(billUpdateDto.getBillId())
+                .count(billUpdateDto.getCount())
+                .name(billUpdateDto.getName())
+                .owner(UserMapper.toUser(billUpdateDto.getOwner()))
                 .build();
     }
 }
