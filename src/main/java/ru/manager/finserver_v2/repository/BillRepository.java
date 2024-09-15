f549abe93c630a0066beb822777c4588472e7219
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-15 21:46:47 +0700

    Добавить в реп метод find all bill

--- src/main/java/ru/manager/finserver_v2/repository/BillRepository.java
+++ src/main/java/ru/manager/finserver_v2/repository/BillRepository.java
@@ -3,5 +3,8 @@ package ru.manager.finserver_v2.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.manager.finserver_v2.model.Bill;
 
+import java.util.List;
+
 public interface BillRepository extends JpaRepository<Bill, Long> {
+    List<Bill> findAllByOwnerUserId(long userId);
 }
