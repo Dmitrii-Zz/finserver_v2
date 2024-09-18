61fc6446bee08c7f179f7ad69684ee85ba9f1b96
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-18 21:29:07 +0700

    Отловить собственные исклюения и исклчение валидации

--- src/main/java/ru/manager/finserver_v2/controller/ErrorHandler.java
+++ src/main/java/ru/manager/finserver_v2/controller/ErrorHandler.java
@@ -2,20 +2,37 @@ package ru.manager.finserver_v2.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.manager.finserver_v2.model.except.BillNotFoundException;
 import ru.manager.finserver_v2.model.except.ErrorResponse;
+import ru.manager.finserver_v2.model.except.NoAccessBillException;
 import ru.manager.finserver_v2.model.except.UserNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler({UserNotFoundException.class, BillNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleUserNotFoundException(final UserNotFoundException e) {
+    public ErrorResponse handlerNotFoundException(final Exception e) {
         log.debug("Произошла ошибка {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({NoAccessBillException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNoAccess(final Exception e) {
+        log.debug("Произошла ошибка {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleValidationException(final Exception e) {
+        log.debug("Ошибка валидации данных: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 }
