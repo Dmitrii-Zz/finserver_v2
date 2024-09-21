a039f15f2522d9b3a4656ae5dd5f5390082af19b
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-21 10:59:17 +0700

    Добавить метод to bill update bill dto

--- src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
+++ src/main/java/ru/manager/finserver_v2/mapper/BillMapper.java
@@ -1,6 +1,7 @@
 package ru.manager.finserver_v2.mapper;
 
 import ru.manager.finserver_v2.dto.BillDto;
+import ru.manager.finserver_v2.dto.BillUpdateDto;
 import ru.manager.finserver_v2.model.Bill;
 
 public class BillMapper {
@@ -20,6 +21,15 @@ public class BillMapper {
                 .count(bill.getCount())
                 .name(bill.getName())
                 .owner(UserMapper.toUserDto(bill.getOwner()))
+                .build();
+    }
+
+    public static BillUpdateDto toBillUpdateBillDto(BillDto billDto) {
+        return BillUpdateDto.builder()
+                .billId(billDto.getBillId())
+                .count(billDto.getCount())
+                .name(billDto.getName())
+                .owner(billDto.getOwner())
                 .build();
     }
 }
