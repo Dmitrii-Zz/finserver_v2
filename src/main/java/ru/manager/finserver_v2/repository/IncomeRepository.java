7da405d4de373f395476061c832752e26ab0b06b
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-22 11:09:46 +0700

    изменить сигнатуру метода get income

--- src/main/java/ru/manager/finserver_v2/repository/IncomeRepository.java
+++ src/main/java/ru/manager/finserver_v2/repository/IncomeRepository.java
@@ -3,6 +3,8 @@ package ru.manager.finserver_v2.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.manager.finserver_v2.model.Income;
 
+import java.util.Optional;
+
 public interface IncomeRepository extends JpaRepository<Income, Long> {
-    Income findByIncomeIdAndOwnerUserId(long incomeId, long userId);
+    Optional<Income> findByIncomeIdAndOwnerUserId(long incomeId, long userId);
 }
