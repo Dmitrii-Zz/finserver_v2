293447ca9b958fb21211eee12ea05fbdb21ac5a4
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-09-22 11:22:12 +0700

    отловить собственное исключение income not found

--- src/main/java/ru/manager/finserver_v2/controller/ErrorHandler.java
+++ src/main/java/ru/manager/finserver_v2/controller/ErrorHandler.java
@@ -6,16 +6,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.manager.finserver_v2.model.except.BillNotFoundException;
-import ru.manager.finserver_v2.model.except.ErrorResponse;
-import ru.manager.finserver_v2.model.except.NoAccessBillException;
-import ru.manager.finserver_v2.model.except.UserNotFoundException;
+import ru.manager.finserver_v2.model.except.*;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler({UserNotFoundException.class, BillNotFoundException.class})
+    @ExceptionHandler({UserNotFoundException.class, BillNotFoundException.class, IncomeNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handlerNotFoundException(final Exception e) {
         log.debug("Произошла ошибка {}", e.getMessage(), e);
@@ -23,14 +20,14 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler({NoAccessBillException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handlerNoAccess(final Exception e) {
         log.debug("Произошла ошибка {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final Exception e) {
         log.debug("Ошибка валидации данных: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
